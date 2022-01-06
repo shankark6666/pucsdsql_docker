18fad051a77108e398fc253df823cbd82ca38955
shankark6666 <60563583+shankark6666@users.noreply.github.com>
2022-01-06 20:40:49 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM mysql
+ENV MYSQL_ROOT_PASSWORD pucsd
+ENV MYSQL_DATABASE pucsdStudents
+ENV MYSQL_USER pucsd
+ENV MYSQL_PASSWORD pucsd
+COPY test.sql /docker-entrypoint-initdb.d/
---
+++ test.sql
@@ -0,0 +1,14 @@
+CREATE TABLE studentData (
+name varchar(25),
+roll_no  varchar(25),
+Address varchar(15),
+mobile_number  varchar(50),
+pan_no varchar(50)
+);
+
+
+
+
+INSERT INTO studentData (name, roll_no, Address, mobile_no,pan_no) 
+VALUES ('Shankar', 'n20111007', 'Pune', '9970666608','LCA01ODG') ;
+
